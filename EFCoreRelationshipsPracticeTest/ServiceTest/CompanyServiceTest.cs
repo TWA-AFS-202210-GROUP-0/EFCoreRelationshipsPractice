3a8d96cb1cd3cb2098da4d1292defc461a9e4cec
Jie Li <JLi147@slb.com>
2022-11-03 18:07:47 +0800

    feat: add ServiceTest Should_create_company_success_via_company_service

---
+++ EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCoreRelationshipsPractice.Dtos;
+using EFCoreRelationshipsPractice.Repository;
+using EFCoreRelationshipsPractice.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EFCoreRelationshipsPracticeTest.ServiceTest
+{
+    [Collection("1")]
+    public class CompanyServiceTest : TestBase
+    {
+        public CompanyServiceTest(CustomWebApplicationFactory<Program> factory)
+            : base(factory)
+        {
+
+        }
+
+        [Fact]
+        public async Task Should_create_company_success_via_company_service()
+        {
+            // given
+            var context = GetCompanyDbContext();
+            CompanyDto companyDto = new CompanyDto();
+            companyDto.Name = "IBM";
+            companyDto.EmployeeDtos = new List<EmployeeDto>()
+            {
+                new EmployeeDto()
+                {
+                    Name = "Tom",
+                    Age = 19,
+                },
+            };
+            companyDto.ProfileDto = new ProfileDto()
+            {
+                RegisteredCapital = 100010,
+                CertId = "100",
+            };
+            CompanyService companyService = new CompanyService(context);
+            // when
+            await companyService.AddCompany(companyDto);
+            // then
+            Assert.Equal(1, context.Companies.Count());
+
+        }
+
+        private CompanyDbContext GetCompanyDbContext()
+        {
+            var scope = Factory.Services.CreateScope();
+            var scopeService = scope.ServiceProvider;
+            CompanyDbContext context = scopeService.GetRequiredService<CompanyDbContext>();
+            return context;
+        }
+    }
+}
