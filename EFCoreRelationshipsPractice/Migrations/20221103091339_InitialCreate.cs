f1d76e40061ac4ae9efcfff2001f8daed2535485
Xiaotian WU <XWu32@slb.com>
2022-11-03 17:31:23 +0800

    test: migrate

---
+++ EFCoreRelationshipsPractice/Migrations/20221103091339_InitialCreate.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreRelationshipsPractice.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "profileEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    RegisteredCapital = table.Column<int>(type: "int", nullable: false),
+                    CertId = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_profileEntities", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "CompanyEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ProfileEntityId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyEntities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CompanyEntities_profileEntities_ProfileEntityId",
+                        column: x => x.ProfileEntityId,
+                        principalTable: "profileEntities",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "employeeEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    CompanyEntityId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employeeEntities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_employeeEntities_CompanyEntities_CompanyEntityId",
+                        column: x => x.CompanyEntityId,
+                        principalTable: "CompanyEntities",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyEntities_ProfileEntityId",
+                table: "CompanyEntities",
+                column: "ProfileEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_employeeEntities_CompanyEntityId",
+                table: "employeeEntities",
+                column: "CompanyEntityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "employeeEntities");
+
+            migrationBuilder.DropTable(
+                name: "CompanyEntities");
+
+            migrationBuilder.DropTable(
+                name: "profileEntities");
+        }
+    }
+}
